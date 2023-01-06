a70334b163ebe95962c766ae4e5c0808cb979fef
KacperStankiewicz <kacper.stankiewicz@marotino.ventures>
2023-01-06 11:40:19 +0100

    initial

---
+++ src/main/java/pl/pja/edu/KDF/KdfApplication.java
@@ -0,0 +1,13 @@
+package pl.pja.edu.KDF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pja/edu/KDF/KdfApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pja.edu.KDF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
