49e4e9faf498f1831706f6ffa9099935b33924f6
aleksander-misztal <misztal.aleksander@gmail.com>
2023-01-07 15:17:59 +0100

    usuniete not nulle z pk

--- src/main/resources/config/liquibase/changelog/20230107144900_create-tables.sql
+++ src/main/resources/config/liquibase/changelog/20230107144900_create-tables.sql
@@ -2,13 +2,13 @@
 --changeset aleksander-misztal:1
 
 CREATE TABLE Authority (
-    id int not null auto_increment primary key ,
+    id int auto_increment primary key ,
     name varchar not null(50),
     description varchar(255)
 );
 
 CREATE TABLE Person (
-    id int not null auto_increment primary key ,
+    id int auto_increment primary key ,
     authority_id int not null REFERENCES Authority(id),
     object_id int not null REFERENCES Object(id),
     name varchar(255) not null,
@@ -19,13 +19,13 @@ CREATE TABLE Person (
 );
 
 CREATE TABLE Allocation (
-    id int not null auto_increment primary key,
+    id int auto_increment primary key,
     person_id int not null REFERENCES Person(id),
     building_id int not null REFERENCES Object(id)
 );
 
 CREATE TABLE Address(
-    id int not null auto_increment primary key,
+    id int auto_increment primary key,
     street not null varchar(255),
     number not null varchar(255),
     city not null varchar(255),
@@ -34,18 +34,18 @@ CREATE TABLE Address(
 );
 
 CREATE TABLE Category (
-    id int not null auto_increment primary key,
+    id int auto_increment primary key,
     name varchar(255) not null
 );
 
 CREATE TABLE Object (
-    id int not null auto_increment primary key,
+    id int auto_increment primary key,
     address int not null REFERENCES Address(id),
     category int not null REFERENCES Category(id)
 );
 
 CREATE TABLE Reservation (
-    id int not null auto_increment primary key,
+    id int auto_increment primary key,
     station_id int not null REFERENCES Station(id),
     date_start TIMESTAMP not null,
     date_end TIMESTAMP not null
@@ -53,7 +53,7 @@ CREATE TABLE Reservation (
 );
 
 CREATE TABLE Station (
-    id int not null auto_increment primary key,
+    id int auto_increment primary key,
     number int not null,
     status bool not null,
     capacity int not null
