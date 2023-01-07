9a0c8a7df83e4a5de2c01ca410a63168f469c3da
aleksander-misztal <misztal.aleksander@gmail.com>
2023-01-07 15:07:19 +0100

    dodanie not nulli

--- src/main/resources/config/liquibase/changelog/20230107144900_create-tables.sql
+++ src/main/resources/config/liquibase/changelog/20230107144900_create-tables.sql
@@ -3,58 +3,58 @@
 
 CREATE TABLE Authority (
     id int not null auto_increment primary key ,
-    name varchar(50),
+    name varchar not null(50),
     description varchar(255)
 );
 
 CREATE TABLE Person (
     id int not null auto_increment primary key ,
-    authority_id int REFERENCES Authority(id),
-    object_id int REFERENCES Object(id),
-    name varchar(255),
-    lastname varchar(255),
-    phone int(9),
-    main varchar(255),
-    address int
+    authority_id int not null REFERENCES Authority(id),
+    object_id int not null REFERENCES Object(id),
+    name varchar(255) not null,
+    lastname varchar(255) not null,
+    phone int(9) not null,
+    main varchar(255) not null,
+    address int not null
 );
 
 CREATE TABLE Allocation (
     id int not null auto_increment primary key,
-    person_id int REFERENCES Person(id),
-    building_id int REFERENCES Object(id)
+    person_id int not null REFERENCES Person(id),
+    building_id int not null REFERENCES Object(id)
 );
 
 CREATE TABLE Address(
     id int not null auto_increment primary key,
-    street varchar(255),
-    number varchar(255),
-    city varchar(255),
-    country varchar(255),
-    postal_code varchar(6)
+    street not null varchar(255),
+    number not null varchar(255),
+    city not null varchar(255),
+    country not null varchar(255),
+    postal_code not null varchar(6)
 );
 
 CREATE TABLE Category (
     id int not null auto_increment primary key,
-    name varchar(255)
+    name varchar(255) not null
 );
 
 CREATE TABLE Object (
     id int not null auto_increment primary key,
-    address int REFERENCES Address(id),
-    category int REFERENCES Category(id)
+    address int not null REFERENCES Address(id),
+    category int not null REFERENCES Category(id)
 );
 
 CREATE TABLE Reservation (
     id int not null auto_increment primary key,
-    station_id int REFERENCES Station(id),
-    date_start TIMESTAMP,
-    date_end TIMESTAMP
+    station_id int not null REFERENCES Station(id),
+    date_start TIMESTAMP not null,
+    date_end TIMESTAMP not null
 
 );
 
 CREATE TABLE Station (
     id int not null auto_increment primary key,
-    number int,
-    status bool,
-    capacity int
+    number int not null,
+    status bool not null,
+    capacity int not null
 )
