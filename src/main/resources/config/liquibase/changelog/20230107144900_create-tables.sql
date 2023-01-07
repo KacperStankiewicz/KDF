767afaf9900b756fbe65ff111ed9e8e8175590b1
KacperStankiewicz <kacper.stankiewicz@marotino.ventures>
2023-01-07 14:55:32 +0100

    Database init

--- src/main/resources/config/liquibase/changelog/20230107144900_create-tables.sql
+++ src/main/resources/config/liquibase/changelog/20230107144900_create-tables.sql
@@ -4,7 +4,7 @@
 CREATE TABLE Authority (
     id int not null auto_increment primary key ,
     name varchar(50),
-    description varchar(255),
+    description varchar(255)
 );
 
 CREATE TABLE Person (
@@ -15,13 +15,13 @@ CREATE TABLE Person (
     lastname varchar(255),
     phone int(9),
     main varchar(255),
-    address int,
+    address int
 );
 
 CREATE TABLE Allocation (
     id int not null auto_increment primary key,
     person_id int REFERENCES Person(id),
-    building_id int REFERENCES Object(id),
+    building_id int REFERENCES Object(id)
 );
 
 CREATE TABLE Address(
@@ -30,12 +30,12 @@ CREATE TABLE Address(
     number varchar(255),
     city varchar(255),
     country varchar(255),
-    postal_code varchar(6),
+    postal_code varchar(6)
 );
 
 CREATE TABLE Category (
     id int not null auto_increment primary key,
-    name varchar(255),
+    name varchar(255)
 );
 
 CREATE TABLE Object (
@@ -48,7 +48,7 @@ CREATE TABLE Reservation (
     id int not null auto_increment primary key,
     station_id int REFERENCES Station(id),
     date_start TIMESTAMP,
-    date_end TIMESTAMP,
+    date_end TIMESTAMP
 
 );
 
