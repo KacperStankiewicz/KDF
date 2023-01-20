9e0b11a19df083c4605212101c1614ccdb1513fb
KacperStankiewicz <kacper.stankiewicz@marotino.ventures>
2023-01-20 21:40:24 +0100

    HOTFIX

--- src/main/java/pl/pja/edu/KDF/Controller/WorkerController.java
+++ src/main/java/pl/pja/edu/KDF/Controller/WorkerController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.pja.edu.KDF.DTO.ReservationCreateDTO;
 import pl.pja.edu.KDF.DTO.ReservationDTO;
 import pl.pja.edu.KDF.Exceptions.BadRequestAlertException;
 import pl.pja.edu.KDF.Service.ReservationService;
@@ -33,18 +32,18 @@ public class WorkerController {
     /**
      * {@code POST  /reservation} : Create a new reservation.
      *
-     * @param reservationCreateDTO the reservationDTO to create.
+     * @param reservationDTO the reservationDTO to create.
      * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new reservationDTO, or with status {@code 400 (Bad Request)} if the reservation has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
     @PostMapping("/worker/reservation")
-    public ResponseEntity<ReservationDTO> createReservation(@Valid @RequestBody ReservationCreateDTO reservationCreateDTO) throws URISyntaxException {
-        log.debug("REST request to save reservation : {}", reservationCreateDTO);
+    public ResponseEntity<ReservationDTO> createReservation(@Valid @RequestBody ReservationDTO reservationDTO) throws URISyntaxException {
+        log.debug("REST request to save reservation : {}", reservationDTO);
 
-        if (reservationCreateDTO.getReservationDTO().getId() != null) {
+        if (reservationDTO.getId() != null) {
             throw new BadRequestAlertException("A new reservation cannot already have an ID", ENTITY_NAME, "idexists");
         }
-        ReservationDTO result = reservationService.save(reservationCreateDTO.getReservationDTO());
+        ReservationDTO result = reservationService.save(reservationDTO);
         return ResponseEntity
                 .created(new URI("/api/reservation/" + result.getId()))
                 .body(result);
